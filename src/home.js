1713b39543fcc2db6f32e8f5ffd524b118327ffa
Haardik Garg <139658164+garghg@users.noreply.github.com>
2025-06-08 08:41:34 -0700

    Update home.js

--- src/home.js
+++ src/home.js
@@ -7,4 +7,50 @@ for (i = 0; i < goals.length; i++){
     span.className  = "close";
     span.appendChild(cross);
     goals[i].appendChild(span);
+};
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++){
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+var list = document.querySelector("ul");
+list.addEventListener("click", function(ev){
+    if (ev.target.tagName === 'LI'){
+        ev.target.classList.toggle('checked');
+    }
+})
+
+
+
+function addTask(){
+    var newTask = document.createElement("li");
+    var input = document.getElementById("input").value;
+    taskName = document.createTextNode(input);
+    newTask.appendChild(taskName);
+    if (input === ''){
+        alert("Empty Input");
+    }
+    else {
+        document.querySelector("ul").appendChild(newTask);
+    }
+
+    document.getElementById("input").value = "";
+
+    var span = document.createElement("SPAN");
+    var cross = document.createTextNode("x");
+    span.className  = "close";
+    span.appendChild(cross);
+    newTask.appendChild(span);
+
+    for (i = 0; i < close.length; i++){
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
 }

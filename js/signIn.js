59a35e8c74425c1c79c3bd03f8214c6c844508a3
Haardik Garg <thehaardikgarg@gmail.com>
2025-06-30 16:38:24 -0700

    signIn page animation

---
+++ js/signIn.js
@@ -0,0 +1,19 @@
+var logo = document.getElementById('logo');
+let scale = 1;
+let opacity = 1;
+
+logo.addEventListener('click', () => {
+    document.getElementById('begin').style.visibility = 'hidden';
+    const interval = setInterval(() => {
+        scale += 1;
+        opacity -= 0.0055;
+
+        logo.style.transform = `scale(${scale})`;
+        logo.style.opacity = opacity;
+
+        if (scale >= 300) {
+            clearInterval(interval);
+            window.open("C:/Users/gargh/OneDrive/Documents/financeManager/html/main.html", "_self");
+        }
+    }, 10);
+});
